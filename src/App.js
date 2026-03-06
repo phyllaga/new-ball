bb63e5c1e07bf2c571644264acfabeee55eb8acd
phyllaga <phyllaga@gmail.com>
2026-03-06 17:55:45 +0800

    1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="container mx-auto py-8">
       <header className="text-center mb-8">
-        <h1 className="text-2xl font-bold">加密货币合约交易工具</h1>
+        <h1 className="text-2xl font-bold">ball</h1>
       </header>
       <Ball />
     </div>
